22f3af8863bfbac1b2438241ec4e99002f7673a5
mrajontech-wq <mrajontech@gmail.com>
2025-12-02 05:06:36 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log('Hello MERN Developer!');
+
+// Variables
+let name = 'MERN Stack';
+console.log(`Welcome to the world of ${name}!`);
+
+// Function
+function add(a, b) {
+    return a + b;
+}
+console.log(`Sum of 5 and 10 is: ${add(5, 10)}`);
+
+// Array
+let user = {
+    name: 'John Doe',
+    age: 30,
+    profession: 'Developer'
+};
+console.log(`User Info: Name - ${user.name}, Age - ${user.age}, Profession - ${user.profession}`);
